fb637e7737c89ff6932fd3bcf5e2a7fca364bcd3
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2024-08-06 10:30:55 +0100

    chore: Add initial project files and configuration

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    )
+}
+
+export default App
