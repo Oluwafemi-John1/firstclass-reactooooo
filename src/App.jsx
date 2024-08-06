55856386d8f3bef77d7a407fb14650708d6d015e
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2024-08-06 10:35:33 +0100

    feat: Add new heading to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from "./components/Navbar"
 const App = () => {
     return (
         <>
+            <h1>This is a new edit</h1>
             <Navbar />
             <Footer />
         </>
